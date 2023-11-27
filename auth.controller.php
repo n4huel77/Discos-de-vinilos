6cc8cb48896bb2707dee6edbb35d47303dd8b4e4
nahuelgulias <145394037+n4huel77@users.noreply.github.com>
2023-11-27 14:42:13 -0300

    Add files via upload

---
+++ auth.controller.php
@@ -0,0 +1,55 @@
+<?php
+require_once './app/views/auth.vista.php';
+require_once './app/models/usuario.modelo.php';
+require_once './app/helpers/auth.helper.php';
+
+
+class AuthController {
+    private $modelo;
+    private $vista;
+
+    function __construct()
+    {
+        $this->modelo = new UsuarioModelo();
+        $this->vista = new AuthVista();
+    }
+    
+    public function mostrarLogin(){
+        
+        $this->vista->mostrarLogin();
+    } 
+
+    public function auth() {
+        $usuario = $_POST['email'];
+        $password = $_POST['pass'];
+        
+        if (empty($usuario) || empty($password)) {
+            $this->vista->mostrarLogin('Faltan completar datos');
+            return;
+        }
+
+        // busco el usuario
+        $user = $this->modelo->getByEmail($usuario);
+   
+        if ($user && password_verify($password,$user->pass)) {
+            
+            AuthHelper::login($user);
+            
+   
+            header('Location: ' . BASE_URL . 'vinilos' );
+        }
+        else {
+            $this->vista->mostrarLogin('Usuario inválido');
+        }
+    }
+
+    public function mostrarAviso(){
+        $this->vista->logoutAviso();
+    }
+    public function logout() {
+        AuthHelper::logout();
+        header('Location: ' . BASE_URL);    
+    }
+
+
+}
